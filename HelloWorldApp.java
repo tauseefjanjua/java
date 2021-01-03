28477de5ab6bf12e913c8504998d77bb2e3bbfa1
tauseefjanjua <tauseef.a.janjua@gmail.com>
2021-01-03 18:44:32 +0500

    first commit

---
+++ HelloWorldApp.java
@@ -0,0 +1,8 @@
+/* The HelloWorldApp class implments and application that simply displays "Hello World!" to the standard output. */
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        // Display the string. No global main
+        System.out.println("Hello World");
+    }
+}
